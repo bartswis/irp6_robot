4033adf98a49288218808c9f0ffc35d6714cab2e
Bartosz Swistak <bartswis@gmail.com>
2015-04-30 12:44:41 +0200

    sarkofag: new launch files + friction

--- sarkofag_regulator/src/SarkofagRegulator.cpp
+++ sarkofag_regulator/src/SarkofagRegulator.cpp
@@ -66,7 +66,6 @@ SarkofagRegulator::SarkofagRegulator(const std::string& name)
       synchro_state_old_(false),
       synchro_state_new_(false),
       type(irp6) {
-
   this->addEventPort(desired_position_).doc(
       "Receiving a value of position step");
   this->addPort(deltaInc_in).doc("Receiving a value of measured increment.");
@@ -129,14 +128,14 @@ void SarkofagRegulator::updateHook() {
         synchro_state_old_ = synchro_state_new_;
       }
     }
-
+    std::cout << desired_position_new_ << std::endl;
     desired_position_increment_ =
         (desired_position_new_ - desired_position_old_)
             * (enc_res_ / (2.0 * M_PI));
 
     if (fabs(desired_position_increment_) > max_desired_increment_) {
       std::cout << "very high pos_inc_: " << reg_number_ << " pos_inc: "
-          << desired_position_increment_ << std::endl;
+                << desired_position_increment_ << std::endl;
 
       emergency_stop_out_.write(true);
     }
@@ -144,119 +143,122 @@ void SarkofagRegulator::updateHook() {
     desired_position_old_ = desired_position_new_;
 
     if (!debug_) {
-          int output;
-          if (synchro_state_old_) {
-            switch (type) {
-              case irp6:
-                output = doServo(desired_position_increment_, deltaIncData);
-                break;
-              case friction_test:
-                output = doServo_friction_test(desired_position_increment_, deltaIncData);
-                break;
-              case pos_inc:
-                output = doServo(desired_position_increment_, deltaIncData);
-                break;
-              default:
-                output = doServo(desired_position_increment_, deltaIncData);
-                break;
-            }
-          } else {
+      int output;
+      if (synchro_state_old_) {
+        switch (type) {
+          case irp6:
             output = doServo(desired_position_increment_, deltaIncData);
-          }
-          /*
-           std::cout << std::dec << GREEN << "output: " << output << " pos_inc: "
-           << desired_position_increment_ << " inp_inc: " << deltaIncData
-           << RESET << std::endl;
-
-           if (iteration_number_ > 2000) {
-           output = 0;
-           }
-           */
-          computedPwm_out.write(output);
-        } else {
-          computedPwm_out.write(0.0);
+            break;
+          case friction_test:
+            output = doServo_friction_test(desired_position_increment_,
+                                           deltaIncData);
+            break;
+          case pos_inc:
+            output = doServo(desired_position_increment_, deltaIncData);
+            break;
+          default:
+            output = doServo(desired_position_increment_, deltaIncData);
+            break;
         }
+      } else {
+        output = doServo(desired_position_increment_, deltaIncData);
+      }
+      /*
+       std::cout << std::dec << GREEN << "output: " << output << " pos_inc: "
+       << desired_position_increment_ << " inp_inc: " << deltaIncData
+       << RESET << std::endl;
+
+       if (iteration_number_ > 2000) {
+       output = 0;
+       }
+       */
+      computedPwm_out.write(output);
+    } else {
+      computedPwm_out.write(0.0);
+    }
   }
 }
 
 int SarkofagRegulator::doServo(double step_new, int pos_inc) {
-    // algorytm regulacji dla serwomechanizmu
-    // position_increment_old - przedostatnio odczytany przyrost polozenie
-    //                         (delta y[k-2] -- mierzone w impulsach)
-    // position_increment_new - ostatnio odczytany przyrost polozenie
-    //                         (delta y[k-1] -- mierzone w impulsach)
-    // step_old_pulse               - poprzednia wartosc zadana dla jednego kroku
-    //                         regulacji (przyrost wartosci zadanej polozenia --
-    //                         delta r[k-2] -- mierzone w impulsach)
-    // step_new               - nastepna wartosc zadana dla jednego kroku
-    //                         regulacji (przyrost wartosci zadanej polozenia --
-    //                         delta r[k-1] -- mierzone w radianach)
-    // set_value_new          - wielkosc kroku do realizacji przez HIP
-    //                         (wypelnienie PWM -- u[k]): czas trwania jedynki
-    // set_value_old          - wielkosc kroku do realizacji przez HIP
-    //                         (wypelnienie PWM -- u[k-1]): czas trwania jedynki
-    // set_value_very_old     - wielkosc kroku do realizacji przez HIP
-    //                         (wypelnienie PWM -- u[k-2]): czas trwania jedynki
+  // algorytm regulacji dla serwomechanizmu
+  // position_increment_old - przedostatnio odczytany przyrost polozenie
+  //                         (delta y[k-2] -- mierzone w impulsach)
+  // position_increment_new - ostatnio odczytany przyrost polozenie
+  //                         (delta y[k-1] -- mierzone w impulsach)
+  // step_old_pulse               - poprzednia wartosc zadana dla jednego kroku
+  //                         regulacji (przyrost wartosci zadanej polozenia --
+  //                         delta r[k-2] -- mierzone w impulsach)
+  // step_new               - nastepna wartosc zadana dla jednego kroku
+  //                         regulacji (przyrost wartosci zadanej polozenia --
+  //                         delta r[k-1] -- mierzone w radianach)
+  // set_value_new          - wielkosc kroku do realizacji przez HIP
+  //                         (wypelnienie PWM -- u[k]): czas trwania jedynki
+  // set_value_old          - wielkosc kroku do realizacji przez HIP
+  //                         (wypelnienie PWM -- u[k-1]): czas trwania jedynki
+  // set_value_very_old     - wielkosc kroku do realizacji przez HIP
+  //                         (wypelnienie PWM -- u[k-2]): czas trwania jedynki
 
-    double step_new_pulse;  // nastepna wartosc zadana dla jednego kroku regulacji
+  double step_new_pulse;  // nastepna wartosc zadana dla jednego kroku regulacji
 
-    step_new_pulse = step_new;
-    position_increment_new = pos_inc;
+  step_new_pulse = step_new;
+  position_increment_new = pos_inc;
 
-    // Przyrost calki uchybu
-    delta_eint = delta_eint_old
-        + (1.0 + eint_dif_) * (step_new_pulse - position_increment_new)
-        - (1.0 - eint_dif_) * (step_old_pulse - position_increment_old);
+  // Przyrost calki uchybu
+  delta_eint = delta_eint_old
+      + (1.0 + eint_dif_) * (step_new_pulse - position_increment_new)
+      - (1.0 - eint_dif_) * (step_old_pulse - position_increment_old);
 
-    // std::cout << "POS INCREMENT NEW: " << position_increment_new <<  std::endl;
+  // std::cout << "POS INCREMENT NEW: " << position_increment_new <<  std::endl;
 
-    // obliczenie nowej wartosci wypelnienia PWM algorytm PD + I
-    set_value_new = (1 + a_) * set_value_old - a_ * set_value_very_old
-        + b0_ * delta_eint - b1_ * delta_eint_old;
+  // obliczenie nowej wartosci wypelnienia PWM algorytm PD + I
+  set_value_new = (1 + a_) * set_value_old - a_ * set_value_very_old
+      + b0_ * delta_eint - b1_ * delta_eint_old;
 
-    // std::cout << "PWM VALUE (" << pos_inc << " to " << pos_inc_new << ") IS " << (int)set_value_new << std::endl;
+  // std::cout << "PWM VALUE (" << pos_inc << " to " << pos_inc_new << ") IS " << (int)set_value_new << std::endl;
 
-    // ograniczenie na sterowanie
-    if (set_value_new > MAX_PWM)
-      set_value_new = MAX_PWM;
-    if (set_value_new < -MAX_PWM)
-      set_value_new = -MAX_PWM;
+  // ograniczenie na sterowanie
+  if (set_value_new > MAX_PWM)
+    set_value_new = MAX_PWM;
+  if (set_value_new < -MAX_PWM)
+    set_value_new = -MAX_PWM;
 
-    if (current_mode_) {
-      output_value = set_value_new * current_reg_kp_;
-      if (output_value > max_output_current_) {
-        output_value = max_output_current_;
-      } else if (output_value < -max_output_current_) {
-        output_value = -max_output_current_;
-      }
-    } else {
-      output_value = set_value_new;
+  if (current_mode_) {
+    output_value = set_value_new * current_reg_kp_;
+    if (output_value > max_output_current_) {
+      output_value = max_output_current_;
+    } else if (output_value < -max_output_current_) {
+      output_value = -max_output_current_;
     }
+  } else {
+    output_value = set_value_new;
+  }
 
-    if (debug_) {
-      std::cout << "output_value: " << output_value << std::endl;
-    }
+  if (debug_) {
+    std::cout << "output_value: " << output_value << std::endl;
+  }
 
-    // przepisanie nowych wartosci zmiennych do zmiennych przechowujacych wartosci poprzednie
-    position_increment_old = position_increment_new;
-    delta_eint_old = delta_eint;
-    step_old_pulse = step_new_pulse;
-    set_value_very_old = set_value_old;
-    set_value_old = set_value_new;
+  // przepisanie nowych wartosci zmiennych do zmiennych przechowujacych wartosci poprzednie
+  position_increment_old = position_increment_new;
+  delta_eint_old = delta_eint;
+  step_old_pulse = step_new_pulse;
+  set_value_very_old = set_value_old;
+  set_value_old = set_value_new;
 
-    return (static_cast<int>(output_value));
+  return (static_cast<int>(output_value));
 }
 
 int SarkofagRegulator::doServo_friction_test(double step_new, int pos_inc) {
   if (pos_inc > 0) {
-    // output_value = 141.6 * pow(position_increment_new, 0.5814) + 1072;
-    output_value = 3888 * exp(0.0004415 * pos_inc)
-        - 2586 * exp(-0.0118 * pos_inc);
+    // output_value = 3300 * exp(0.000964 * pos_inc) - 2529 * exp(-0.02009 * pos_inc);
+    // output_value = 3751 * exp(0.0005574 * pos_inc) - 2429 * exp(-0.01222 * pos_inc);
+    output_value = 3747.035989910421 * exp(0.0007760029070562380 * pos_inc)
+        - 2427.127443613446 * exp(-0.009736098716195 * pos_inc);
   }
   if (pos_inc < 0) {
-    // output_value = 141.6 * pow(-position_increment_new, 0.5814) + 1072;
-    output_value = 3888 * exp(0.0004415 * (-pos_inc))
-        - 2586 * exp(-0.0118 * -(pos_inc));
+    // output_value = 3300 * exp(0.000964 * (-pos_inc)) - 2529 * exp(-0.02009 * -(pos_inc));
+    // output_value = 3751 * exp(0.0005574 * (-pos_inc)) - 2429 * exp(-0.01222 * -(pos_inc));
+    output_value = 3747.035989910421 * exp(0.0007760029070562380 * (-pos_inc))
+        - 2427.127443613446 * exp(-0.009736098716195 * (-pos_inc));
     output_value = -output_value;
   }
 
@@ -269,7 +271,7 @@ int SarkofagRegulator::doServo_friction_test(double step_new, int pos_inc) {
     output_value = -max_output_current_;
   }
   std::cout << "pos_inc: " << pos_inc << "   output_value: " << output_value
-      << std::endl;
+            << std::endl;
 
   if (debug_) {
     std::cout << "output_value: " << output_value << std::endl;
